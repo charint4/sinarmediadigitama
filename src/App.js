eb5c9bc8ce8e597b16ce4662bff59a016de3b970
rafiharahap <rafi.harahaps@gmail.com>
2022-09-22 15:39:42 +0700

    update navbar and logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Navigation from './Components/Navigation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navigation />
         <h1>halo ges</h1>
       </header>
     </div>
