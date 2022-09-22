4ed345cbcd4aebe41b30fbbf906834d88c200a67
rafiharahap <rafi.harahaps@gmail.com>
2022-09-22 16:13:59 +0700

    edit navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Navigation />
+        <section>
+          
+        </section>
+        <section>
+
+        </section>
         <h1>halo ges</h1>
       </header>
     </div>
--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -5,30 +5,26 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Logo from '../../Images/smd-logo.png';
 import '../Navigation/nav.css';
+import Button from 'react-bootstrap/Button';
+
 
 function BasicExample() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className="p-3">
       <Container>
         <Navbar.Brand href="#home" >
           <img src={Logo} className="nav-logo"></img>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="me-auto nav-btn">
+            <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
+            <Nav.Link href="#link" className="nav-link">About Us</Nav.Link>
+            <Nav.Link href="#link" className="nav-link">Services</Nav.Link>
+          
+          </Nav>
+          <Nav>
+            <Button className="contact-btn px-4">Contact Us</Button>{' '}
           </Nav>
         </Navbar.Collapse>
       </Container>
