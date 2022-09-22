6220898f50dec21bdcf89f30100892ecdcd86328
rafiharahap <rafi.harahaps@gmail.com>
2022-09-23 04:47:28 +0700

    added section 1 - 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 import Navigation from './Components/Navigation';
 import Section1 from './Components/Section1';
 import Section2 from './Components/Section2';
+import Section3 from './Components/Section3';
 
 function App() {
   return (
@@ -10,8 +12,10 @@ function App() {
       <header className="App-header">
         <Navigation />
         <section className="sec-1-3">
+          <Header />
           <Section1 />
           <Section2 />
+          <Section3 />
         </section>
         <section>
 
