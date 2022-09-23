a92d537461c001f70f1c04905a265cc21b140482
charint4 <rafif.ridhoo@gmail.com>
2022-09-23 15:26:18 +0700

    add section 4

--- src/Components/Section4/index.js
+++ src/Components/Section4/index.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import map from "../../Images/map.jpg";
 import "../Section4/section4.css";
 
 const Section4 = () => {
-  return <div>Section4</div>;
+  return (
+    <Container>
+      <div className="d-flex justify-content-center align-item-center sec4">
+        <div className="map">
+          <img src={map} alt="map" />
+        </div>
+        <div className="bg-text">
+          <h3 className="title">Get in touch</h3>
+          <h4 className="text1">
+            Jl. Aria Putra No. 8 RT 01 / RW 06 Kel. Sarua Indah, Kec. Ciputat,
+            Tangerang Selatan 15414
+          </h4>
+          <h4 className="text2">
+            bagus.sms@gmail.com sinarmediasakti.adv@gmail.com
+            baliho.sms@gmail.com
+          </h4>
+          <h4 className="text3">Telp / Fax 021 - 74634075 - 74633530</h4>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Section4;
