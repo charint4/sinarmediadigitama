44e7e882d482e88052604f21ffdec307ff7b4e3e
charint4 <rafif.ridhoo@gmail.com>
2022-09-24 20:27:53 +0700

    add aboutUs section2

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Section1 from "../Components/AboutUs/Section1abt";
+import Section2 from "../Components/AboutUs/Section2";
 import Section3abt from "../Components/AboutUs/Section3abt";
 import Section4abt from "../Components/AboutUs/Section4abt";
 import Section5abt from "../Components/AboutUs/Section5abt";
@@ -8,6 +9,7 @@ const AboutUs = () => {
   return (
     <div className="">
       <Section1 />
+      <Section2 />
       <Section3abt />
       <Section4abt />
       <Section5abt />
