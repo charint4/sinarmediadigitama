ebd8d085e1f8f207c7215e5d5795dc0b000cf06b
charint4 <rafif.ridhoo@gmail.com>
2022-09-24 20:53:10 +0700

    add header services

---
+++ src/Components/Services/Header/index.js
@@ -0,0 +1,32 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import "./header.css";
+import Button from "react-bootstrap/Button";
+import asset1 from "../../../Images/bi_arrow-down-right.png";
+import asset2 from "../../../Images/Group.png";
+import asset3 from "../../../Images/digitama-new-full.png";
+
+const Header = () => {
+  return (
+    <>
+      <div className="wrap-head">
+        <Container>
+          <div className="row container-head">
+            <div className="col-md cont-head">
+              <img align="middle" className="img-client" src={asset3}></img>
+            </div>
+            <div className="col-md cont-head">
+              <img align="middle" className="img-client" src={asset1}></img>
+            </div>
+            <div className="col-md cont-head">
+              <h2>just call us</h2>
+              <img align="middle" className="img-client" src={asset2}></img>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Header;
--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../Components/Services/Header";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <>
+      <Header className="mb-100" />
+    </>
+  );
 };
 
 export default Services;
