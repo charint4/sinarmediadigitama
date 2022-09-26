8405400b47f0b1bd8f4fbd13f8b76eb64b73b722
naufalrafi-hub <rafi.harahaps@gmail.com>
2022-09-26 17:28:53 +0700

    adding image slider

--- src/Components/Services/Section2serv/index.js
+++ src/Components/Services/Section2serv/index.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 import Slider from "react-slick";
 import "./section2serv.css"
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import frontOffice from "../../../Images/frontoffice.jpg";
+import kitchenOffice from "../../../Images/kitchen.jpg";
+import insideOffice from "../../../Images/Insideoffice.jpg";
+import roomOffice from "../../../Images/roomoffice.jpg";
+import meetOffice from "../../../Images/meetoffice.jpg";
 // import "~slick-carousel/slick/slick.css";
 // import "~slick-carousel/slick/slick-theme.css";
 
@@ -14,10 +21,41 @@ export default class CenterMode extends Component {
       centerPadding: "60px",
       slidesToShow: 3,
       speed: 500
+      
     };
+    const responsive = {
+      0: { items: 1 },
+      568: { items: 2 },
+      1024: { items: 3 },
+  };
+    const handleDragStart = (e) => e.preventDefault();
+    const items = [
+      <div className="item" data-value="1">
+        <img src={frontOffice}>
+        </img>
+      </div>,
+      <div className="item" data-value="2">
+        <img src={kitchenOffice}>
+        </img>
+      </div>,
+      
+      <div className="item" data-value="3">
+        <img src={meetOffice}>
+      </img>
+      </div>,
+      <div className="item" data-value="4">
+        <img src={insideOffice}>
+        </img>
+      </div>,
+      <div className="item" data-value="5">
+        <img src={roomOffice}>
+        </img>
+      </div>,
+  ];
+  
     return (
       <Container className="slider-foto">
-      <div>
+      {/* <div>
         <h2>Center Mode</h2>
         <Slider {...settings}>
           <div>
@@ -39,7 +77,14 @@ export default class CenterMode extends Component {
             <h3>6</h3>
           </div>
         </Slider>
-      </div>
+      </div> */}
+      <AliceCarousel
+        mouseTracking
+        items={items}
+        // paddingLeft={50}
+        // paddingRight={50}
+        responsive={responsive}
+    />
       </Container>
     );
   }
