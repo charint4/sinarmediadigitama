66803135de1082b7d2c3132a67c9be58a4f6a71d
naufalrafi-hub <rafi.harahaps@gmail.com>
2022-09-29 03:30:01 +0700

    fix arrow responsive layout on header section landing page

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import './header.css';
 import Button from 'react-bootstrap/Button';
 import asset1 from '../../Images/bi_arrow-down-right.png';
+import arrow from '../../Images/arrow-down.png';
 import asset2 from '../../Images/Group.png';
 import asset3 from '../../Images/digitama-new-full.png';
 
@@ -16,7 +17,8 @@ const Header = () => {
                         <img align="middle" className="img-client" src={asset3}></img>
                     </div>
                     <div className="col-md cont-head">
-                        <img align="middle" className="img-client" src={asset1}></img>
+                        <img align="middle" className="img-client ar-left" src={asset1}></img>
+                        <img align="middle" className="img-client ar-down" src={arrow}></img>
                     </div>
                     <div className="col-md cont-head">
                         <h2>just call us</h2>
