1ced920511bd0e5f87cd4ca512266042c9cf24a2
charint4 <rafif.ridhoo@gmail.com>
2022-10-01 17:06:04 +0700

    create condition if products is zero

--- src/Components/Services/Section1serv/index.js
+++ src/Components/Services/Section1serv/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import "./section1serv.css";
-// import services from "../../../Images/services.png";
+import services from "../../../Images/services.png";
 
 const index = (props) => {
-  const {data} = props 
+  const { data } = props;
   return (
     <Container>
       <div className="row wrap-services">
         <div className="col-md bg-blur p-5 cont-services">
-          <h3>
-            {data.description}
-          </h3>
+          <h3>{data.description}</h3>
         </div>
         <div className="col-md cont-services d-flex flex-wrap align-items-center justify-content-center">
-          <img src={data.products[0].img} alt="services"/>
+          <img
+            src={data.products.length ? data.products[0].img : services}
+            alt="services"
+          />
         </div>
       </div>
     </Container>
