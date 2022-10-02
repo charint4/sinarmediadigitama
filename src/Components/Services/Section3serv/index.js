ebceb42b8820d32300f85effb27cd42ec51aa445
charint4 <rafif.ridhoo@gmail.com>
2022-10-03 00:59:14 +0700

    remove comment

--- src/Components/Services/Section3serv/index.js
+++ src/Components/Services/Section3serv/index.js
@@ -5,24 +5,12 @@ import "./section3serv.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useRef } from "react";
-import { useParams } from "react-router-dom";
 import { useDraggable } from "react-use-draggable-scroll";
 
 const MultipleItems = (props) => {
   const { data } = props;
   const ref = useRef();
   const { events } = useDraggable(ref);
-  // console.log(id);
-  //   console.log(this.props.data.products.map((product, index) => (
-  //     <div className="item wrap-imga bg-blur p-3" data-value={index+1} key={index}>
-  //          <img className="img-abt" src={product.img}>
-  //          </img>
-  //          <h1 className="fs-20">{product.title}</h1>
-  //      </div>
-  //     )))
-
-  // console.log(data.products.length > 0);
-  // console.log(data.products.length);
 
   return (
     <Container className={data.products.length === 0 ? "d-none" : ""}>
@@ -42,7 +30,6 @@ const MultipleItems = (props) => {
               data-value={index + 1}
               key={index}
             >
-              {/* <h1 className="text-center"> Our Products </h1> */}
               <img
                 variant="top"
                 src={product.img}
