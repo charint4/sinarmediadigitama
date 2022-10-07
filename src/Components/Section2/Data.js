2220aa1615bf3b266b13a691337cc3579c98dc16
rafiharahap <rafi.harahaps@gmail.com>
2022-10-07 18:15:32 +0700

    change image on services

--- src/Components/Section2/Data.js
+++ src/Components/Section2/Data.js
@@ -19,16 +19,30 @@ import printComp from "../../Images/webp/printed-comp.webp";
 import sosmedComp from "../../Images/webp/sosmed-comp.webp";
 import creComp from "../../Images/webp/cre-comp.webp";
 import Camol from "../../Images/webp/camol.webp";
+import oohSGM from "../../Images/billboard-sgm.jpg"
+import oohSweety from "../../Images/billboard-sweety.jpg"
+import creDev1 from "../../Images/creative-dev.jpg"
+import creDev2 from "../../Images/creative-dev2.jpg"
+import evEnt from "../../Images/eventz-1.jpg"
+import evEnt2 from "../../Images/eventz-2.jpg"
+import soCial from "../../Images/socmed.jpg"
+import soCial2 from "../../Images/socmed2.jpg"
+import inFlue from "../../Images/influencers-1.jpg"
+import inFlue2 from "../../Images/influencers-2.jpg"
+import printMat from "../../Images/printedmaterials.jpg"
+import printMat2 from "../../Images/printedmaterials2.jpg"
+import promUnit from "../../Images/promotional-units.jpg"
+import promUnit2 from "../../Images/promotional-units2.jpg"
 
 export default [
   {
     name: "Out Of Home Units",
-    ava: Camol,
+    ava: oohSGM,
     texts: "nationwide",
     quotes: "read more",
     description:
       "We understand that choosing the right strategy to promote your brand is pivotal. We can assist you in selecting the best advertisement placements to increase your visibility within your budget. Our multiple OOH platforms provide nationwide coverage for brands looking to advertise in cities areas and generate high traffic through our advertising platforms.",
-    ava2: oohComp,
+    ava2: oohSweety,
     products: [
       {
         img: OOh2,
@@ -42,12 +56,12 @@ export default [
   },
   {
     name: "Promotional Units",
-    ava: Promo,
+    ava: promUnit,
     texts: "paper, woods, acrylic, metal based",
     quotes: "read more",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
-    ava2: promoComp,
+    ava2: promUnit2,
     products: [
       {
         img: Product2,
@@ -73,12 +87,12 @@ export default [
   },
   {
     name: "Creative Development Promotional Units",
-    ava: Crepromo2,
+    ava: creDev1,
     texts: "Digitally advertised",
     quotes: "read more",
     description:
       "Eye-catching and memorable branding can hook loyal consumers. Manufacturing branding is not merely projecting an image to customers about what your company stands for. It’s about letting your consumer understand its meaning in making your quality product to the market. Good branding can add a human touch to your brand and make it more relatable. Over time, the image your brand conveys will grow into trust, and customers will come to rely on your products as the better option that distinguishes you from your competitors. Every entrepreneur understands that a product presentation is vital for their sales strategy. We are here to help you maximize the visibility of your branding booth and POSM while staying within your budget.",
-    ava2: creComp,
+    ava2: creDev2,
     products: [
       {
         img: Product3,
@@ -96,12 +110,12 @@ export default [
   },
   {
     name: "Printed Material",
-    ava: Printed,
+    ava: printMat,
     texts: "offset, screen print, digital print",
     quotes: "read more",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
-    ava2: printComp,
+    ava2: printMat2,
     products: [
       {
         img: Product5,
@@ -119,12 +133,12 @@ export default [
   },
   {
     name: "Events",
-    ava: Event,
+    ava: evEnt,
     texts: "Indoor, outdoor, and digital events",
     quotes: "read more",
     description:
       "Offering an intimate and engaging atmosphere with your loyal and potential consumers can be created through events. Events provide an opportunity to make a lasting impression through direct interaction between your brand and consumers. A successful event can strengthen positive relationships between clients, prospects, consumers, and employees. We can deliver product launches and corporate, business, and social events to help your brand shine.",
-    ava2: eventComp,
+    ava2: evEnt2,
     products: [
       {
         img: Product1,
@@ -142,12 +156,12 @@ export default [
   },
   {
     name: "Influencer",
-    ava: Influe,
+    ava: inFlue,
     texts: "bringing you the best brand deals",
     quotes: "read more",
     description:
       "The power of word of mouth can influence consumer behavior and engagement in their purchase. Today, most buying behavior and brand awareness are driven by influencers. Therefore, its critical to find influencers that fit your needs. We can help you find the best Talent, KOLs, and Influencers for your advertising campaign. Not only do we find the best, but we also help you manage the talents and ensure the highest quality of all outputs.",
-    ava2: infComp,
+    ava2: inFlue2,
     products: [
       {
         img: Product2,
@@ -165,12 +179,12 @@ export default [
   },
   {
     name: "Social media management",
-    ava: Sosmed,
+    ava: soCial,
     texts: "Instagram, tiktok, youtube",
     quotes: "read more",
     description:
       "We believe creative content is a fundamental asset in communicating brand awareness to your audience. Crafting creative content increases audience trust and conversions across all channels. Through our services, we will help you navigate the three phases of social media management that cover strategic planning, post & content management, and monitoring & reporting.",
-    ava2: sosmedComp,
+    ava2: soCial2,
     products: [
       {
         img: Product3,
