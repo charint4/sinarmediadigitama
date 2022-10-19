1e8eeeaf39bc7a5bbb50f320a1b424a7ea839808
rafiharahap <rafi.harahaps@gmail.com>
2022-10-19 22:28:31 +0800

    add data on our hist section

---
+++ src/Components/AboutUs/Section2/Data.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        head : "Our History",
+        first : "Started as a small agency that serves printing materials and promotional units (acrylic, plastic, and wood-based)",
+        second : "Expanded range of services to include metal based units",
+        third : "Built larger workshops because of significance increase in number of projects",
+        fourth : "Established Sinar Media Digitama as the umbrella company that builds creative and agile business processes using better systems and technology",
+        onw : "Onwards"
+    }
+]
