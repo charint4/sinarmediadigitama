574fa8e5b4d7fec0331a1550d983d4e51f807395
naufalrafi-hub <rafi.harahaps@gmail.com>
2022-10-27 20:09:37 +0700

    change data

--- src/Components/AboutUs/Section2/Data.js
+++ src/Components/AboutUs/Section2/Data.js
@@ -6,5 +6,6 @@ export default [
         third : "Built larger workshops because of the significant increase in the number of projects",
         fourth : "Established Sinar Media Digitama as the umbrella company that builds creative and agile business processes using better systems and technology",
         onw : "Onwards"
-    }
+    },
+    
 ]
