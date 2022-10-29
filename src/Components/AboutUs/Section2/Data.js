8ba8e90196ba62401ad754c54e2873e0bd9688a1
naufalrafi-hub <rafi.harahaps@gmail.com>
2022-10-29 12:58:18 +0700

    add some cw to data

--- src/Components/AboutUs/Section2/Data.js
+++ src/Components/AboutUs/Section2/Data.js
@@ -7,5 +7,13 @@ export default [
         fourth : "Established Sinar Media Digitama as the umbrella company that builds creative and agile business processes using better systems and technology",
         onw : "Onwards"
     },
+    {
+        head : "History Kami",
+        first : "Diawali dengan terbentuknya agency kecil yang menawarkan pelayanan bahan percetakan akrilik, plastik, dan material berbasis kayu",
+        second : "Memperluas jenis layanan yang kami berikan dengan mencakup unit berbasis logam",
+        third : "Membangun workshop yang lebih besar untuk mendukung proses pengerjaan jumlah proyek yang semakin meningkat. ",
+        fourth : "Mendirikan Sinar Media Digitama sebagai payung perusahaan yang membangun proses bisnis kreatif dan cepat menggunakan sistem dan teknologi yang lebih baik.",
+        onw : "Onwards"
+    }
     
 ]
--- src/Components/AboutUs/Section3abt/Data.js
+++ src/Components/AboutUs/Section3abt/Data.js
@@ -8,5 +8,15 @@ export default [
         fourth : "Choosing the best placement for your ad is crucial for your brand and product. Successful ad placement will elevate your brand awareness and visibility. Allow us to assist you in choosing the best option for your marketing strategy.",
         fifth : "Years of Experiences",
         sixth : "Our experience in the advertising industry has helped brands attain larger and more targeted audiences. We understand what the audience desires to convey a unique brand."
+    },
+    {
+        head : "Why Us?",
+        subhead : "What makes us different?",
+        first : "One stop service",
+        second : "Solusi One Stop Marketing muncul dari keinginan kami untuk menjadi agency pemasaran yang komprehensif dengan memberikan hasil yang memuaskan kepada klien kami, terlepas dari layanan yang dibutuhkan.",
+        third : "360 Approach",
+        fourth : "Menentukan penempatan iklan yang baik merupakan hal yang sangat penting untuk brand dan produk. Hal tersebut selain akan meningkatkan kesadaran juga dapat mengembangkan visibilitas brand. Perkenankan kami untuk siap bantu memilih opsi terbaik untuk strategi pemasaranmu.",
+        fifth : "Years of Experiences",
+        sixth : "Pengalaman kami dalam industri periklanan telah membantu beberapa brand menggapai audiens yang lebih luas dan sesuai. Kami memahami apa yang diinginkan audiens untuk menyampaikan brand yang unik."
     }
 ]
