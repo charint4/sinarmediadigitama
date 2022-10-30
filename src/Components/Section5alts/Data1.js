29e147868259b80947ac4647cbcc09fb64d85b40
charint4 <rafif.ridhoo@gmail.com>
2022-10-31 01:26:33 +0700

    add new Data1

---
+++ src/Components/Section5alts/Data1.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    title: "Meet Our Team",
+    desc: "We are led by a group of highly motivated people with diverse backgrounds and extensive experience to unlock creativity around every corner.",
+  },
+  {
+    title: "Tim Kami",
+    desc: "Sekelompok profesional yang bermotivasi tinggi dengan latar belakang yang beragam, pengalaman luas, minat khusus, dan rekam jejak kesuksesan. Perbedaan inilah yang saling melengkapi keterampilan yang kami miliki.",
+  },
+];
