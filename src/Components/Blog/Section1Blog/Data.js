35c87e198914869e7647275580a7bbbafdf10710
naufalrafi-hub <rafi.harahaps@gmail.com>
2022-11-16 04:39:37 +0700

    add photos on blog

--- src/Components/Blog/Section1Blog/Data.js
+++ src/Components/Blog/Section1Blog/Data.js
@@ -1,4 +1,6 @@
 import Hashtag from "./../../../Images/webp/hashtag.webp"
+import Marketing from "./../../../Images/webp/marketing.webp"
+import Tiktokvig from "./../../../Images/tiktokvsig.jpg"
 
 export default [
     {
@@ -9,13 +11,13 @@ export default [
         headart: "Digital Marketing Sangat Penting untuk UMKM, Inilah Alasannya. Banyak dari kamu yang mungkin saat ini ingin atau baru memulai sebuah bisnis, entah makanan, pakaian hingga barang dan jasa lainnya. Dalam perkembangan dunia saat ini, kamu sebagai pemilik bisnis harus berlomba-lomba juga untuk memiliki strategi digital marketing yang tepat. Menjalankan strategi digital marketing memberikanmu banyak keuntungan dan ini menjadi hal yang penting untuk bisa kamu aplikasikan di bisnismu, apa saja?"
     },
     {
-        img: Hashtag,
+        img: Marketing,
         title: '5 Pilar Strategi Marketing yang Harus Brand Miliki', 
         sub: 'blabla',
         read: 'read more'
     },
     {
-        img: Hashtag,
+        img: Tiktokvig,
         title: 'TikTok vs Instagram, mana yang paling tepat untuk bisnismu?', 
         sub: 'blabla',
         read: 'read more'
