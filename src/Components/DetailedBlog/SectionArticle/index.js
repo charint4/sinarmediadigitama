040712504d615fcd3dc5e587422a1742c3611906
naufalrafi-hub <rafi.harahaps@gmail.com>
2022-11-21 04:30:14 +0700

    fix \n bug

--- src/Components/DetailedBlog/SectionArticle/index.js
+++ src/Components/DetailedBlog/SectionArticle/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
+import "./Article.css"
 // import Data from '../../AboutUs/SectionQt/Data'
 
 const SectionArticle = (props) => {
@@ -7,8 +8,8 @@ const SectionArticle = (props) => {
   return (
     <Container>
     {data.para.map((paragh, index) => (
-      <div className="wrap-art mt-60" key={index}> {/* use mapping keanya sabi*/}
-        <p className="mt-40">{paragh.par}</p>
+      <div className="wrap-art mt-50" key={index}> {/* use mapping keanya sabi*/}
+        <p className="mt-20">{paragh.par}</p>
         {/* <p className="mt-40">{data.par2}</p>
         <p className="mt-40">{data.par3}</p>
         <p className="mt-40">{data.par4}</p>
--- src/Components/DetailedBlog/SectionBlogNav/index.js
+++ src/Components/DetailedBlog/SectionBlogNav/index.js
@@ -4,7 +4,7 @@ import "./Blognav.css"
 const SectionBlogNav = () => {
   return (
     <div className='navBlog mt-150'>
-      <p>Share This Article</p>
+      <p>Share Article</p>
       <h3>01</h3>
       <h3>02</h3>
       <h3>03</h3>
